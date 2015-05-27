4215a337533e0bdefa97d0ac0e6d226093b86744
Phelipe Perboires <phelipe.perboires@gmail.com>
2015-05-27 13:17:26 -0300

    Move InternalVisibleTo to another file.

---
+++ src/SqlMigrator/Properties/InternalVisibleTo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Integration")]
+[assembly: InternalsVisibleTo("UnitTests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
